35d543a000c5dbc5bdca58352dc801df7cabe834
BenjaminHerault <148880832+BenjaminHerault@users.noreply.github.com>
2024-09-23 11:39:29 +0200

    Fin TriDeNombre

--- 01-Algorithmes/TriDeNombre/Program.cs
+++ 01-Algorithmes/TriDeNombre/Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace TriDeNombre{
+    internal class Program{
+        
+        static void Main(string[] args){
+            /* VARIABLES */
+
+            int nombreA,nombreB,nombreC;
+
+            /* DEBUT PROGRAMME */
+
+            Console.WriteLine("Entrez un nombre A : ");
+            nombreA = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entrez un nombre B : ");
+            nombreB = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entrez un nombre C : ");
+            nombreC = int.Parse(Console.ReadLine());
+
+                /*DEBUT du grand si*/
+
+                 if(nombreA<=nombreB && nombreA<=nombreC)
+                {
+                    /*DEBUT petit si 1*/
+                    if(nombreB<=nombreC){
+                        Console.WriteLine(nombreA+ "<= "+ nombreB+ "<=" + nombreC);
+                    }
+                        else{
+                            Console.WriteLine(nombreA+ "<="+ nombreC + "<=" + nombreB);
+                        }
+                }
+                    /*FIN petit si 1*/
+                        else if (nombreB <= nombreA && nombreB <= nombreC)
+                        {
+                            /*DEBUT petit si 2*/
+                            if( nombreA <= nombreC){
+                                Console.WriteLine(nombreB+ "<=" + nombreA+ "<="+nombreC );
+                            }
+                            else{
+                                Console.WriteLine(nombreB+ "<="+ nombreC+"<="+ nombreA );
+                            }
+                            /*fin du petit si 2 */
+                        }
+                        else
+                        {
+                            /*DEBUT du petit si 3*/
+                            if(nombreA <=nombreB){
+                                Console.WriteLine( nombreC+"<="+ nombreA+"<="+nombreB);
+                            }
+                            else{
+                                Console.WriteLine(nombreC +"<="+nombreB+"<="+nombreA);
+                            }
+                            /*FIN petit si 3*/
+                        }
+                 /*FIN du grand si*/
+            /* FIN PROGRAMME */
+        }
+    }
+}
+
