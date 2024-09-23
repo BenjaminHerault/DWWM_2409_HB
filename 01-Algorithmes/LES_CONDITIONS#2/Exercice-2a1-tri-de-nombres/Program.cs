f4cc999919df66ba2f0a3296f601ea8ceb0e8c79
BenjaminHerault <148880832+BenjaminHerault@users.noreply.github.com>
2024-09-23 15:40:59 +0200

    Les conditions Exercice 2a1
    J'ai fait la premier version de l'exercice 2a1

---
+++ 01-Algorithmes/LES_CONDITIONS#2/Exercice-2a1-tri-de-nombres/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("J'ai besoin d'une valeur !");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("J'ai besoin d'une valeur !");
+int B = int.Parse(Console.ReadLine());
+Console.WriteLine("\n");
+
+int[] triNombre = new int[] {
+    A,B
+};
+
+Array.Sort(triNombre);
+foreach(int i in triNombre){
+    Console.WriteLine(i);
+}
