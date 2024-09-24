123d2455e0712fa98b3ce3facfb7370a9df696a6
BenjaminHerault <148880832+BenjaminHerault@users.noreply.github.com>
2024-09-24 12:04:55 +0200

    Creaction de project et deplacemnt des project

---
+++ 01-Algorithmes/Algoritmes/EsTuMajeur/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Variable*/
+
+int JoursNaissance;
+int MoisNaissance;
+int AnneeNaissance;
+
+/*Traitement*/
+DateTime localDate = DateTime.Now;
+
+Console.WriteLine("donner moi votre jours de naissance");
+JoursNaissance = int.Parse(Console.ReadLine());
+
+Console.WriteLine("donner moi votre mois de naissance");
+MoisNaissance = int.Parse(Console.ReadLine());
+
+Console.WriteLine("donner moi votre année de naissance");
+AnneeNaissance = int.Parse(Console.ReadLine());
+
+Console.WriteLine(localDate.Year);
+
