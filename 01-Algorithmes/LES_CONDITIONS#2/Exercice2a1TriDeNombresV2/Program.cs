c8af143e089d2bf7038e905b53f4cac3d7adf5a6
BenjaminHerault <148880832+BenjaminHerault@users.noreply.github.com>
2024-09-24 08:29:46 +0200

    Exercice 2a1 Tri De Nombres V2
    j'ai refait l'exercice avec de si et des sinon

---
+++ 01-Algorithmes/LES_CONDITIONS#2/Exercice2a1TriDeNombresV2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exercice2a1TriDeNombresV2{
+    internal class Program{
+        static void Main(string[] args){
+
+            /*VARIABLES*/
+            int A,B;
+
+            /*DEBUT PROGRAMME*/
+
+            Console.WriteLine("Entrez une valeur pour A");
+            A = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entrez une valeur pour B");
+            B = int.Parse(Console.ReadLine());
+
+            /*DEBUT SI*/
+            if (A<B){
+                Console.WriteLine(A +" < "+B);
+            }
+                else{
+                    Console.WriteLine(B+" < "+A);
+                }
+        }
+    }
+}
