2aaa06baeb6bd51a0874f887b53c1af3bd8639c2
BenjaminHerault <148880832+BenjaminHerault@users.noreply.github.com>
2024-09-24 16:30:03 +0200

    Exercice  ma retraite fini

--- 01-Algorithmes/Algoritmes/MaRetraite/Program.cs
+++ 01-Algorithmes/Algoritmes/MaRetraite/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Variable */
+int age = 0;
+int ageRetraite = 60;
+int anneeRetraite;
+
+/*Traitement*/
+Console.WriteLine("Saisi votre âge");
+
+try
+{
+    age = int.Parse(Console.ReadLine());
+}
+catch(Exception ex)
+{
+    Console.WriteLine("L'age fourni n'est pas valide ! " + ex.Message);
+    Environment.Exit(1);
+}
+
+
+/*Affichage*/
+
+if (age>60)
+{
+    anneeRetraite = age - ageRetraite;
+    Console.WriteLine("Vous êtes à la retraite depuis "+anneeRetraite+" années");
+}
+else if (age < 60)
+{
+    anneeRetraite = ageRetraite - age;
+    Console.WriteLine("Il vous reste "+anneeRetraite+" années avant la retraite.");
+}
+else
+{
+    Console.WriteLine("C'est le moment de prendre sa retraite");
+}
+
+Console.WriteLine();
