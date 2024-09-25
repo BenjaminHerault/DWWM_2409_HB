be28caa45f17c053f17a78abb959bd0533ef1f61
BenjaminHerault <148880832+BenjaminHerault@users.noreply.github.com>
2024-09-25 14:39:03 +0200

    Exercice controler la saisie fini

--- 01-Algorithmes/Algoritmes/Contrôler la saisie/Program.cs
+++ 01-Algorithmes/Algoritmes/Contrôler la saisie/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Varible */
+string prenom;
+bool prenomOk;
+/*Traitement*/
+
+do
+{
+    prenomOk = true;
+    Console.WriteLine("Saisir votre prénom");
+    prenom = Console.ReadLine();
+    if (prenom.Length < 2)
+    {
+        Console.WriteLine("Il faut minimum 2 caractères !");
+        prenomOk = false;
+    }
+    else if (!prenom.All(char.IsLetter))
+    {
+        Console.WriteLine("Merci de mettre que des lettres"!);
+        prenomOk = false;
+    } 
+}
+while ( !prenomOk);
+Console.WriteLine(prenom);
