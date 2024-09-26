8bfa8e7a252d2bb7ea515eed2b15a475c5a8377f
BenjaminHerault <148880832+BenjaminHerault@users.noreply.github.com>
2024-09-26 12:28:23 +0200

    Exercice nombre premier en cours / rajout de l'exercice recherche des divisurs d'un nombre

--- 01-Algorithmes/Algoritmes/NombrePremier/Program.cs
+++ 01-Algorithmes/Algoritmes/NombrePremier/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿/*Varible*/
+int nombrePremier;
+
+/*Traitement*/
+Console.WriteLine("Donner moi un nombre a tester pour savoir si il est premier !");
+nombrePremier = int.Parse(Console.ReadLine());
+
+
