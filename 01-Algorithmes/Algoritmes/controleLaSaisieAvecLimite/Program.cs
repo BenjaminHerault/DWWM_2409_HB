d61344587fd5ec14ee8fd5f8ea386be92266776c
BenjaminHerault <148880832+BenjaminHerault@users.noreply.github.com>
2024-09-26 10:21:21 +0200

    Exercice contrôler la saisie avec limite fini et fonctionnel

--- 01-Algorithmes/Algoritmes/controleLaSaisieAvecLimite/Program.cs
+++ 01-Algorithmes/Algoritmes/controleLaSaisieAvecLimite/Program.cs
@@ -1,16 +1,33 @@
 ﻿/*Varible*/
+using System.Numerics;
+
 string motDePasse = "formation";
 string saisirUtilisateur;
-
+bool isOk;                      // c'est ok
+int compteur =0;
 
 /*Traitement*/
-Console.WriteLine("Saisir un mot de passe");
-saisirUtilisateur = Console.ReadLine();
-motDePasse.Equals (saisirUtilisateur);
-Console.WriteLine(motDePasse);
 
-for(int i = 0; i < 3; i++)
+do
 {
-
+    Console.WriteLine("Saisir un mot de passe");
+    saisirUtilisateur = Console.ReadLine();
+    isOk = motDePasse.Equals(saisirUtilisateur);
+    compteur++;
+    /*Affichage*/
+    if (!isOk && compteur <3)
+    {
+        Console.WriteLine("mauvais mot de passe, " + compteur + "/3");
+        isOk = false;
+    }
+    else if (!isOk && compteur < 4)
+    {
+        Console.WriteLine("Votre compte est bloqué");
+    }
+    else
+    {
+        Console.WriteLine("Vous êtes connecté");
+        isOk=true;
+    }
 }
-
+while (!isOk && compteur < 3 );
