2077ef37b88cb19535ebf6fcb2c4afbbbf829539
BenjaminHerault <148880832+BenjaminHerault@users.noreply.github.com>
2024-09-26 11:49:25 +0200

    Exercice Intervalle entre 2 nombres fini

--- 01-Algorithmes/Algoritmes/IntervalleEntre2Nombres/Program.cs
+++ 01-Algorithmes/Algoritmes/IntervalleEntre2Nombres/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Varible*/
+int A;
+int B;
+
+/*Traitement */
+Console.WriteLine("Donner moi un nombre entier pour A");
+A = int.Parse(Console.ReadLine());
+Console.WriteLine("Donner moi un nombre entier pour B");
+B = int.Parse(Console.ReadLine());
+
+while (A < B-1)
+{
+    A ++;
+    Console.Write(A+" ");
+}
+while (A > B+1)
+{
+    A--;
+    Console.Write(A + " ");
+}
+
+
+
+
