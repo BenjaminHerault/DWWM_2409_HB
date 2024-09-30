b71344cac253a1f700e1bac7befb3902bfa7c706
BenjaminHerault <148880832+BenjaminHerault@users.noreply.github.com>
2024-09-30 16:10:32 +0200

    exercice tableaux fini

--- 01-Algorithmes/Algoritmes/tableauxPartieCinq/Program.cs
+++ 01-Algorithmes/Algoritmes/tableauxPartieCinq/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Variable*/
+int[] tableau = new int[4];
+bool isOk;
+int N ;
+
+/*Traitement*/
+for (int i = 0; i <= tableau.Length - 1; i++)              // Pour crée ma serie de nombre dans le tableau
+{
+    Console.WriteLine("Rentre la premier valeur du tableux qui sera en index :" + i);
+    tableau[i] = int.Parse(Console.ReadLine());
+}
+Array.Sort(tableau);                                   // mais les nombre par ordre croissant
+for (int j = 0;j < tableau.Length; j++)                // pour afficher le tableau 
+{
+    //Console.Write(tableau[j] + " ");                 // afficher le tableaux 
+    Console.WriteLine("Donnez moi un chiffre a vérifier dans le tableau ");
+    isOk = int.TryParse(Console.ReadLine(),out N);
+    if (!isOk)
+    {
+        Console.WriteLine("j'ai besoin d'un nombre pour fonctionner");
+    }
+    if (N == tableau[j]) 
+    {
+        Console.WriteLine("Voice l'index de votre nombre " +j);
+    }
+    else
+    {
+        Console.WriteLine("Nombre non trouvé");
+    }
+}
+
+
