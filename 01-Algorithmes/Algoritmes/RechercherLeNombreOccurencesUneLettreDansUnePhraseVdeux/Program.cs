f09b15ee6c29e7618a20718c28427226bf3c9c2f
BenjaminHerault <148880832+BenjaminHerault@users.noreply.github.com>
2024-10-01 16:02:51 +0200

    exercie 5.2 fonctionnel

--- 01-Algorithmes/Algoritmes/RechercherLeNombreOccurencesUneLettreDansUnePhraseVdeux/Program.cs
+++ 01-Algorithmes/Algoritmes/RechercherLeNombreOccurencesUneLettreDansUnePhraseVdeux/Program.cs
@@ -1,46 +1,44 @@
-﻿/*Varible*/
-string laPhrase = "";
+﻿string laPhrase ="";
 string rechereche = "";
 int compteur = 0;
+string verifiacationPoin;
 
-/*Traitement*/
-
-Console.WriteLine("Donnez moi une phrase");
+Console.WriteLine("écrivez un message");
 laPhrase = Console.ReadLine().ToLower();
 
-if (laPhrase[laPhrase.Length - 1] == '.' )
+if (laPhrase.EndsWith("."))   //EndsWith = Détermine si la fin de cette instance de chaîne correspond à une chaîne spécifiée.
 {
     if (laPhrase.Length > 1)
     {
-        Console.WriteLine("Donne moi une lettre a verrifier  ");
+        Console.WriteLine("Donne moi une lettre a vérifier ");
         rechereche = Console.ReadLine().ToLower();
+
         for (int i = 0; i < laPhrase.Length; i++)
         {
-            if (laPhrase[i] == char.Parse(rechereche)) // char.Parse(), int.Parse()... pour convertir
+            if (laPhrase[i] == char.Parse(rechereche))// char.Parse(), int.Parse()... Convertit la valeur de la chaîne spécifiée en son caractère Unicode équivalent.
             {
                 compteur++;
             }
         }
     }
+    else
+    {
+        Console.WriteLine("LA CHAINE EST VIDE");
+    }
+    if (compteur == 0 && laPhrase != "")
+    {
+        Console.WriteLine($"La lettre \"{rechereche}\" n’est pas présente");
+    }
+    else
+    {
+        Console.WriteLine($"Il a {compteur} occurrences avec la lettre \"{rechereche}\".");
+    }
 }
-else
+else if(string.IsNullOrEmpty(laPhrase))
 {
-    Console.WriteLine("la phrase se termine PAS par un .");
+    Console.WriteLine("LA CHAINE EST VIDE");  // IsNullOrEmpty = Indique si la chaîne spécifiée est null ou une chaîne vide ("")
 }
-//La phrase est vide
-if (laPhrase == "" || laPhrase == ".")
+else 
 {
-    Console.WriteLine("je suis vide");
-}
-
-//La lettre n’est pas présente
-else if (compteur == 0 && laPhrase != "")
-{
-    Console.WriteLine("pas la lettre");
-}
-
-//La lettre est présente une ou plusieurs fois
-else
-{
-    Console.WriteLine(compteur);
+    Console.WriteLine("il manque le \".\" ");
 }
