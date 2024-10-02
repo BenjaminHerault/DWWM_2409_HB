f706293e03e2cf917001d0ab908b6f79cddc8d2a
BenjaminHerault <148880832+BenjaminHerault@users.noreply.github.com>
2024-10-02 15:22:55 +0200

    exercice mot de passe

--- 01-Algorithmes/Algoritmes/DenombrerLesLettresDeLalphabetDansUnTableau/Program.cs
+++ 01-Algorithmes/Algoritmes/DenombrerLesLettresDeLalphabetDansUnTableau/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Variable*/
+string leTexte;
+string[] recherche;
+int occurences;
+
+/*Pour ranger un tableaux* avec une boucle for/
+//string abc = "abcdefghijklmnopqrstuvwxyz";
+//string[] rangementABC;
+/*rangementABC = new string[26];
+for (int i = 0; i < abc.Length; i++)
+{
+    renchementABC[i]=abc[i].ToString();
+    Console.WriteLine(renchementABC[i]);
+}*/
+
+/*Traitement*/
+try
+{
+    /*declaration du tableau*/
+    recherche = new string[] { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+    Console.WriteLine("Ecrivez un texte de 120 caractères (à contrôler) ");
+    leTexte = Console.ReadLine().ToLower();
+}
+
+catch(Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+
---
+++ 01-Algorithmes/Algoritmes/LesCapitales/Program.cs
@@ -0,0 +1,53 @@
+﻿/*Créez une fonction « capitalCity() » qui accepte un argument de type string (le pays dont on cherche la capitale).
+Elle devra retourner le nom de la capitale des pays suivants :
+France => Paris
+Allemagne => Berlin*/
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string? pays;
+        string capitale;
+
+        Console.WriteLine("Saisissez un nom de pays: ");
+
+        pays = Console.ReadLine();
+
+        capitale = CapitalCity(pays);
+
+        Console.WriteLine(capitale);
+    }
+
+    static string CapitalCity(string _pays)
+    {
+        /* if (_pays == "France")
+           {
+               return "Paris";
+           }
+           else if (_pays == "Allemagne")
+           {
+               return "Berlin";
+           }
+           else 
+           {
+               return "Capitale inconus";
+           }*/
+
+        string resulat ;
+
+        switch (_pays)
+        {
+            case "France":
+ /* return */   resulat = "Paris";
+                break;
+            case "Allemagne":
+                resulat = "Berlin";
+                break;
+            default:
+                resulat ="Capital inconnue";
+                break;
+        }
+        return resulat;
+    } 
+}
