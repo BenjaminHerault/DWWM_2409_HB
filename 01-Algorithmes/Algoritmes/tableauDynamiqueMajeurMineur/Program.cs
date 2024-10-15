f1fe502e2bdb4f1b7ee9b46d000714de2d6dd316
BenjaminHerault <148880832+BenjaminHerault@users.noreply.github.com>
2024-10-15 08:50:59 +0200

    exercice mineure majeur plus nom prenom fait avec ameloration plus rajoute de commentaire

--- 01-Algorithmes/Algoritmes/tableauDynamiqueMajeurMineur/Program.cs
+++ 01-Algorithmes/Algoritmes/tableauDynamiqueMajeurMineur/Program.cs
@@ -1,35 +1,37 @@
 ﻿/*Variable*/
-using System.Net.WebSockets;
 using System.Text.RegularExpressions;
-using System.Threading.Channels;
 
-string NomPrenom = "";
+
+string nomPrenom = "";
 string metierOuCouleur = "";
 string ageString = "";              // Je converti age qui et en int en string  
 string maDateString = "";           // Je converti dateNaissance qui et en int en string 
 string dateNaissance = "";     
 int age = 0;
 char saisieOuiNon;
-Regex regexVerificationNomPrenom ;
 bool nomValide = true;
 
 // craction de ma liste utilisateur qui sera fortement typée
-// List <List<string>> quand on aura plusier list et List <string> quand on a une list !  
+// List <List<string>> quand on aura plusier list et List <string> quand on a une list !
+// Une list normal ressemble a sa List List<string>utilisateurs = new List<string>();
 List<List<string>> utilisateurs = new List<List<string>>();
 
 /*Regex implemantation*/
+Regex regexVerificationNomPrenom;
+// on assigne une valeur a la regex 
 regexVerificationNomPrenom = new Regex(@"^[a-zàâéèëêïîôöùüûçñ -]+$", RegexOptions.IgnoreCase);
 /*Traitement*/
 // debut du do while
-// Pour demandele nom et le prenom de la personne 
-
 do
 {
+    //On verifier que le nom et correct 
     do
     {
+        // Pour demandele nom et le prenom de la personne 
         Console.WriteLine("Saisier un nom et prénom.");
-        NomPrenom = Console.ReadLine() ?? "";
-        nomValide = regexVerificationNomPrenom.IsMatch(NomPrenom);
+        nomPrenom = Console.ReadLine() ?? "";
+        //IsMatch = Indique si l’expression régulière trouve une correspondance dans la chaîne d’entrée.
+        nomValide = regexVerificationNomPrenom.IsMatch(nomPrenom);
         if (!nomValide)
         {
             Console.WriteLine("Format invalide.");
@@ -41,21 +43,26 @@ do
     Console.WriteLine("Saisiez une date de naissance (01/01/2000");
     dateNaissance = Console.ReadLine()??"";
     // debut de si
-    if (DateTime.TryParse(dateNaissance, out DateTime maDate)) //DateTime = Représente un instant, généralement exprimé sous la forme d’une date ou d’une heure.
+
+    //DateTime = Représente un instant, généralement exprimé sous la forme d’une date ou d’une heure.
+    if (DateTime.TryParse(dateNaissance, out DateTime maDate)) 
     {
-        DateTime dateActuelle = DateTime.Now;   // Pour recupere la date d'aujourd'hui avec DateTime.Now
-        TimeSpan ageEnIntervaleDeTemps = dateActuelle - maDate;  //TimeSpan = Représente un intervalle de temps. Pour avoir une valeur precise 
-        age = (int)(ageEnIntervaleDeTemps.Days/365.25);     //Pour cacluer l'age en annee on utilise un Cast = Pour converti le TimeSpan.Days en int
+        // Pour recupere la date d'aujourd'hui avec DateTime.Now
+        DateTime dateActuelle = DateTime.Now;
+        //TimeSpan = Représente un intervalle de temps. Pour avoir une valeur precise
+        TimeSpan ageEnIntervaleDeTemps = dateActuelle - maDate;
+        //Pour cacluer l'age en annee on utilise un Cast = Pour converti le TimeSpan.Days en int
+        age = (int)(ageEnIntervaleDeTemps.Days/365.25);     
         if(age <18)
         {
             Console.WriteLine("Quelle est votre couleur préferée.");
-            metierOuCouleur = Console.ReadLine() ?? "";
         }
         else
         {
             Console.WriteLine("Quelle est votre métier");
-            metierOuCouleur = Console.ReadLine() ?? "";
         }
+        // Comment on a deux demande qui prennet la meme variable on peux assigner la valeur a la fin pour eviter les repetition
+        metierOuCouleur = Console.ReadLine() ?? "";
     }
     else
     {
@@ -64,15 +71,18 @@ do
 
     //  On converti un int en string avec ToString
     ageString = age.ToString();       
-    // ToLongDateString = Convertit la valeur de l'objet DateTime actuel en sa représentation sous forme de chaîne de date longue équivalente.
-    // Mecredi 1 mai 1996
-    maDateString = maDate.ToLongDateString();      
+    // ToLongDateString = Convertit la valeur de
+    // l'objet DateTime actuel en sa représentation sous forme de chaîne de date longue équivalente.
+    // execmple : Mecredi 1 mai 1996
+    maDateString = maDate.ToLongDateString();
 
-    // [] le numero de chaque objects premier object qui commence a 0   [] les objects avec leurs attributs qui commence a 0 
-    utilisateurs.Add([NomPrenom, maDateString, ageString, metierOuCouleur]);
+    // utilisateurs et une liste et on lui donne comme valeur  nomPrenom, maDateString, ageString, metierOuCouleur
+    utilisateurs.Add([nomPrenom, maDateString, ageString, metierOuCouleur]);
 
+    //On demander si on toi rajouter un utilisateur 
     Console.WriteLine("Voulez vous rajouter un autre utilisateur ? Non(n) ou Oui (o)");
-    saisieOuiNon = Console.ReadKey(true).KeyChar;    // pour appyer sur un touche et valide le choix la toucher appuier va pas apparaietre 
+    // pour appyer sur un touche et valide le choix la toucher appuier va pas apparaietre 
+    saisieOuiNon = Console.ReadKey(true).KeyChar;    
 
 } while (saisieOuiNon == 'o' || saisieOuiNon == 'O');
 
