53c4aa056d819fb0eef9a909dbe04e673db199a6
BenjaminHerault <148880832+BenjaminHerault@users.noreply.github.com>
2024-11-18 11:40:36 +0100

    site de sport fini pour la parti pc

--- 02-WebFront/siteSport/assets/JS/style.js
+++ 02-WebFront/siteSport/assets/JS/style.js
@@ -12,18 +12,6 @@ img.onerror = function() {
 img.src = 'assets/image/carteDeFrance/Carte_France.png';
 
 
-
-// // Fonction pour ajouter des icônes
-// function addIcon(x, y, iconSrc) {
-//     const icon = new Image();
-//     icon.onload = function() {
-//         ctx.drawImage(icon, x - icon.width / 2, y - icon.height / 2);
-//     };
-//     icon.onerror = function() {
-//         console.error('Erreur de chargement de l\'icône.');
-//     };
-//     icon.src = 'assets/image/icone/icon.png';
-
      
 // Fonction pour ajouter des icônes
 function addIcon(x, y, iconSrc) {
@@ -39,13 +27,14 @@ function addIcon(x, y, iconSrc) {
  
 // Ajouter des icônes aux positions souhaitées
 const positions = [
-    {x: 100, y: 150, src: 'assets/image/icone/icon1.png'},
-    {x: 200, y: 250, src: 'assets/image/icone/icon2.png'},
-    {x: 300, y: 100, src: 'assets/image/icone/icon3.png'},
-    {x: 400, y: 300, src: 'assets/image/icone/icon4.png'},
-    {x: 500, y: 200, src: 'assets/image/icone/icon5.png'},
-    {x: 600, y: 400, src: 'assets/image/icone/icon6.png'},
-    {x: 700, y: 500, src: 'assets/image/icone/icon7.png'}
+    {x: 520, y: 135, src: 'assets/image/icone/icon1.png'},
+    {x: 430, y: 242, src: 'assets/image/icone/icon2.png'},
+    {x: 170, y: 262, src: 'assets/image/icone/icon3.png'},
+    {x: 735, y: 270, src: 'assets/image/icone/icon4.png'},
+    {x: 170, y: 364, src: 'assets/image/icone/icon5.png'},
+    {x: 534, y: 490, src: 'assets/image/icone/icon6.png'},
+    {x: 562, y: 690, src: 'assets/image/icone/icon7.png'},
+    {x: 210, y: 520, src: 'assets/image/icone/icon6.png'}
 ];
  
 positions.forEach(function(pos) {
