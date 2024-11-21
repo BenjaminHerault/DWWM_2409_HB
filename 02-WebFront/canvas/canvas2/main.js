a3a19820b23e452b20c768ff8ebda2a1a5e0ec4e
BenjaminHerault <148880832+BenjaminHerault@users.noreply.github.com>
2024-11-21 14:45:43 +0100

    canvas emple en cour

---
+++ 02-WebFront/canvas/canvas2/main.js
@@ -0,0 +1,13 @@
+
+const canvas = document.getElementById("canvas");
+let context = canvas.getContext("2d");
+
+var window_height = window.innerHeight;
+var window_width = window.innerWidth;
+
+canvas.width = window_width;
+canvas.height = window_height;
+
+canvas.style.backgroundColor = "#ff8";
+
+
