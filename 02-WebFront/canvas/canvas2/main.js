cc1b88760350d25035446fb477388d842ae42fc1
BenjaminHerault <148880832+BenjaminHerault@users.noreply.github.com>
2024-11-22 10:58:24 +0100

    pour envoyer

--- 02-WebFront/canvas/canvas2/main.js
+++ 02-WebFront/canvas/canvas2/main.js
@@ -10,4 +10,8 @@ canvas.height = window_height;
 
 canvas.style.backgroundColor = "#ff8";
 
+class Circle {
+    constructor(xpos, ypos, radius, color)
+}
+// https://www.youtube.com/watch?v=EutvXK3eh0I&list=PLN0tvDAN1yvSNbkHAwPzJ5O4pP_e2vyme&index=3&ab_channel=BananaCoding
 
