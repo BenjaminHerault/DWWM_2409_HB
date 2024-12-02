ba0a2d4d11dbba595853350031e5e55fab279c08
BenjaminHerault <148880832+BenjaminHerault@users.noreply.github.com>
2024-12-02 14:06:23 +0100

    rajout des correction des cours

---
+++ 01-Algorithmes/Algoritmes/correction/01 - Algorithmes/TriDeNombre/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using MdevoldereTools;
+
+Console.WriteLine("Tri de Nombres ++");
+
+// VARIABLES
+
+int numA;
+int numB;
+int numC;
+int[] nombres;
+
+// TRAITEMENT
+
+numA = ConsoleTools.DemanderNombreEntier("Entrez le premier nombre");
+numB = ConsoleTools.DemanderNombreEntier("Entrez le deuxième nombre");
+numC = ConsoleTools.DemanderNombreEntier("Entrez le troisième nombre");
+
+nombres = [numA, numB, numC];
+
+Array.Sort(nombres);
+
+// AFFICHAGE
+
+    for (int i = 0; i < nombres.Length; i++)
+    {
+        Console.WriteLine(nombres[i]);
+    }
+    
+Console.ReadLine();
