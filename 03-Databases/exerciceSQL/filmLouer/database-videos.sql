b82233c5aac47f9373467ae1071026aa3da9cb53
BenjaminHerault <148880832+BenjaminHerault@users.noreply.github.com>
2024-12-18 12:54:15 +0100

    cours plus avion

--- 03-Databases/exerciceSQL/filmLouer/database-videos.sql
+++ 03-Databases/exerciceSQL/filmLouer/database-videos.sql
@@ -1,50 +1,72 @@
 /*commentaire*/
 -- commentaire
+/*
+Création de la base de données
+Sous langage : DDL / LDD
+Date Definition Language
+Langage de définition des données 
+Principales instruction : CREATE / ALTER / DROP
+CREATE = créer une structure (DATABASE, TABLE, VIEW, PROCEDURE, TRIGGER, FUNCTION)
+ALTER = Modifier une structure existante
+DROP = Supprimer une structure existante
+*/
 
 /* SUPPRIMER LA BASE DE DONNEES SI ELLE EXISTE */
 /*IF EXISTS pour eviter l'erreure*/
 DROP DATABASE IF EXISTS videos;
 
 /*creer une base de donnees nommee "videos"*/
+-- create database videos;
 create database if not exists videos;
 
-/*utiliser la base de donnees cree*/
+/*Selectionner / utiliser la base de donnees cree*/
 
 -- les requetes qui suivent utiliseront
 -- la base de donnees selectionne ci-dessus
 use videos;
 
-/* creer une table nommee "film"*/
-create table if not exists film
+-- Les requêtes qui suivent utiliseront
+-- la base de donnés sélectionné ci-dessou
+
+
+/* CREER une table nommee" realisateur" */
+CREATE TABLE realisateur
 (
-	film_id int auto_increment primary key,
-    film_titre varchar(255) not null,
-    film_duree smallint not null
+	realisateur_id INT PRIMARY KEY AUTO_INCREMENT, 
+    realisateur_nom VARCHAR(100) NOT NULL,
+    realisateur_prenom VARCHAR(100) NOT NULL
 );
-/* Inserer le jeu d'essai dans la table film*/
-insert into film 
-value
-(null,"Léon",120),
-(null,"E.T",90),
-(null,"ça", 103);
 
-insert into film 
-(film_titre, film_duree)
-value
-("L'exorciste",120),
-("Super Papa",87),
-("Gladiator 2", 117);
+CREATE TABLE acteur
+(
+	acteur_id INT AUTO_INCREMENT,
+    acteur_nom VARCHAR(100) NOT NULL,
+    acteur_prenom VARCHAR(100) NOT NULL,
+    PRIMARY KEY (acteur_id)
+);
 
-insert into film 
-(film_duree, film_titre)
-value
-(98, "Loups-Garous");
+CREATE TABLE film
+(
+	film_id INT AUTO_INCREMENT,
+    film_titre VARCHAR(255) NOT NULL,
+    film_duree SMALLINT NOT NULL,
+    realisateur_id  INT,
+    PRIMARY KEY (film_id),
+    FOREIGN KEY (realisateur_id) REFERENCES realisateur(realisateur_id)
+);
 
-
+CREATE TABLE film_acteur
+(
+	film_id INT,
+    acteur_id INT,
+    PRIMARY KEY(film_id, acteur_id),
+    FOREIGN KEY (film_id) REFERENCES film(film_id),
+    FOREIGN KEY (acteur_id) REFERENCES acteur(acteur_id)
+);
 
 
 /*Afficher les donnees de la table*/
 
-select * from film;
+/*select * from film;
 
-select film_id, film_titre from film;
+select film_id, film_titre from film;*/
---
+++ cours/03 - Databases/02 Exercice Videos/02d-videos-requetes-select.sql
@@ -0,0 +1,16 @@
+/* REQUETES SQL A IMPLEMENTER */
+
+/* 1. Sélectionner tous les films (titre, duree) */
+
+/* 2. Sélectionner le film n°3 (identifiant, titre, duree) */
+
+/* 3. Sélectionner tous les films triés par durée du plus long au plus court */
+
+/* 4. Sélectionner tous les acteurs (identifiant, nom, prénom) triés par nom (ordre alphabétique) */
+
+/* 5. Sélectionner l'acteur dont le nom est "Réno" */
+
+/* 6. Sélectionner les réalisateurs (nom, prénom) triés par nom et par ordre décroissant */
+
+/* 7. Sélectionner les films (titre, durée) avec pour chacun d'entre eux, le nom et prénom du réalisateur */
+
