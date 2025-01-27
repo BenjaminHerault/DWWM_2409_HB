27bbf3f42af764be549f4ce10bd54f1430aa9762
BenjaminHerault <148880832+BenjaminHerault@users.noreply.github.com>
2025-01-27 08:03:48 +0100

    pour push

--- 03-Databases/SQL/TP2_SQL/sql/03_TP_architecture_DML_exercice.sql
+++ 03-Databases/SQL/TP2_SQL/sql/03_TP_architecture_DML_exercice.sql
@@ -99,8 +99,101 @@ INNER JOIN type_travaux ON projets.type_travaux_id = type_travaux.type_travaux_i
 INNER JOIN type_projets ON projets.type_projet_id = type_projets.type_projet_id;
 
 /* 10. Sélectionner les projets dont l'adresse est identique au client associé */
+SELECT projet_ref, type_projet_libelle, projets.adresse_id, client_nom, concat(adresse_code_postal," ", adresse_ville," ",adresse_num_voie," ", adresse_voie) AS "adresses"
+FROM projets
+INNER JOIN type_projets ON projets.type_projet_id = type_projets.type_projet_id
+INNER JOIN clients ON projets.client_ref = clients.client_ref
+INNER JOIN adresses ON clients.adresse_id = adresses.adresse_id
+WHERE projets.adresse_id = clients.adresse_id;
 
 
 
+
+
+
+
+-- afficher les projets d'un architecte --- Pour un nom d'atchitecte en variable,  donner la reference des projets dont il est responsable (verifier sa fonction)
+-- 
+-- delimiter |
+-- CREATE PROCEDURE RechercheProjets(IN nom_emp VARCHAR(50)) 
+-- BEGIN
+-- SELECT projet_ref,fonctions.fonction_nom
+-- FROM projets
+-- INNER JOIN employes ON projets.emp_matricule=employes.emp_matricule NATURAL JOIN fonctions
+-- WHERE employes.emp_nom=nom_emp; 
+-- END|
+-- 
+-- DELIMITER ;
+-- 
+-- SET @nom_employes:="roussotte";
+-- 
+-- CALL RechercheProjetsparArchitecte( @nom_employes);
+-- CALL RechercheProjetsparArchitecte("Golay");
+-- 
+-- CALL RechercheProjets("Roussotte");
+
+-- afficher_liste_projet_fonction
+
+
+
+-- Créer une PROCEDURE stockée qui pour un nom de salarié renvoie
+-- dans une variable le budget total des projets dont il est responsable, 
+-- et renvoie 0 si pas de projet en responsabilité  
+
+DELIMITER |
+CREATE PROCEDURE budgetTotal(IN nom_emp VARCHAR(50), OUT totalBudget DECIMAL(10,2), OUT nbProjets INT)
+BEGIN
+SELECT fonctions.fonction_nom 
+FROM fonctions
+NATURAL JOIN employes
+WHERE employes.emp_nom = nom_emp;
+
+SELECT IFNULL(SUM(projet_prix),0) INTO totalBudget
+FROM projets
+INNER JOIN employes ON employes.emp_matricule = projets.emp_matricule
+WHERE employes.emp_nom = nom_emp;
+
+SELECT IFNULL(count(projet_ref),0) INTO nbProjets
+FROM projets
+INNER JOIN employes ON employes.emp_matricule = projets.emp_matricule
+WHERE employes.emp_nom = nom_emp;
+
+END|
+DELIMITER ;
+
+SET @nom := "Golay";
+
+CALL budgetTotal(@nom, @montant, @nb);
+
+SELECT @montant AS "Somme des projets";
+SELECT @nb AS "Nombre de projets";
+
+
+
+-- definir un variable qui sera le cumul des montants projets  @cumul_projet_test
+-- definir une "stored procedure" qui en fonction du numero de projet choisi, ajoutera son montant au @cumul_projet_test pour avoir le montant global
+
+
+DELIMITER |
+CREATE PROCEDURE ajouterBudgetProj(IN numero_projet INT , INOUT cumul_projet DECIMAL(10,2) )
+BEGIN
+SELECT  (cumul_projet + projets.projet_prix) INTO cumul_projet FROM projets WHERE projet_ref= numero_projet; 
+
+END|
+DELIMITER ;
+
+
+
+SELECT @cumul_projet3 AS  "depart";
+ 
+
+CALL ajouterBudgetProj( 4, @cumul_projet3);
+
+SELECT @cumul_projet3 AS  "résultat intermédiaire";
+
+CALL ajouterBudgetProj( 2, @cumul_projet3);
+
+
+SELECT @cumul_projet3 AS "résultat final";
 
 
