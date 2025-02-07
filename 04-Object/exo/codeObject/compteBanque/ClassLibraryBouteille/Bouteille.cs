04b68d3ec2a2cbed167f23923aece5d905ac3a08
BenjaminHerault <148880832+BenjaminHerault@users.noreply.github.com>
2025-02-07 11:38:05 +0100

    bouteille cod en cours

--- 04-Object/exo/codeObject/compteBanque/ClassLibraryBouteille/Bouteille.cs
+++ 04-Object/exo/codeObject/compteBanque/ClassLibraryBouteille/Bouteille.cs
@@ -1,11 +1,13 @@
-﻿namespace ClassLibraryBouteille
+﻿using static System.Reflection.Metadata.BlobBuilder;
+
+namespace ClassLibraryBouteille
 {
     public class Bouteille
     {
         //attributs
-        private double contenanceEnLitre;
-        private double contenuEnLitre;
-        private bool estOuverte;
+        private double contenanceEnLitre { get; init; }
+        private double contenuEnLitre { get; set; }
+        private bool estOuverte { get; set; }
 
         //constructeurs
 
@@ -23,11 +25,52 @@
             this.contenuEnLitre = contenuEnLitre;
             this.estOuverte = estOuverte;
         }
-        
+        //constructeur hybride classique defaut
         public Bouteille(double contenanceEnLitre,
             double contenuEnLitre)
             :this(contenanceEnLitre,contenuEnLitre,false)
         {
         }
+        //constructeur hybride classique autre exemple
+        public Bouteille(double contenanceEnLitre)
+            :this(contenanceEnLitre, contenanceEnLitre, false)
+        {
+        }
+
+        //constructeur par clonage
+        public Bouteille(Bouteille bouteilleACopier)
+        {
+            this.contenanceEnLitre = bouteilleACopier.contenanceEnLitre;
+            this.contenuEnLitre = bouteilleACopier.contenuEnLitre;
+            this.estOuverte = bouteilleACopier.estOuverte;
+        }
+
+        public bool Ouvrir()                
+        {
+            if (estOuverte == true)     // si estOuverte et vrai
+            {
+                return false;           // alors la méthode Ouvrir() retourne faux 
+            }                           // car on peux pas la réouvrir
+            else
+            {
+                estOuverte = true;       // sinon la bouteille et fermer 
+                return true;            // on l'ouvre et la méthode Ouvrir() retourner vrai 
+            } 
+        }
+        public bool Fermer()
+        {
+            if (estOuverte == true)     //si estOuverte est vrai
+            {                           //alors la méthode Fermer() retourne vrai
+                return true;
+            }                           //car on peux fermer la bouteille
+            else
+            {                           //sinon la bouteille et fermer
+                return false;           //on peux pas la re fermer
+            }    
+        }
+        //public bool Vider()
+        //{
+        //    if(estOuverte == true && )
+        //}
     }
 }
--- 04-Object/exo/codeObject/compteBanque/ConsoleAppBouteille/Program.cs
+++ 04-Object/exo/codeObject/compteBanque/ConsoleAppBouteille/Program.cs
@@ -1,11 +1,21 @@
 ﻿using ClassLibraryBouteille;
 
 
-
-    Bouteille b;
+//contenanceEnLitre contenuEnLitre estOuverte 
+Bouteille b;
 
 b = new Bouteille();
 
 b = new Bouteille(2,1.5,false);
+
+b = new Bouteille(2,2,false);
+
+//Bouteille clone = new Bouteille(b);
+
+// Pour la méthode Ouvrir 
+bool testOuvire = b.Ouvrir();
+
+// Pour la méthode fermer
+//bool testFermer = b.Fermer();
 
 int a = 0;
