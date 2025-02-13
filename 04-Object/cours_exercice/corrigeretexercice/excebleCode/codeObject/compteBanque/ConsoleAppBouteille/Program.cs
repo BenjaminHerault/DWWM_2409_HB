da6d6f6437f552b0af076a7a69caecbde412fa81
BenjaminHerault <148880832+BenjaminHerault@users.noreply.github.com>
2025-02-13 10:06:46 +0100

    rangement

---
+++ 04-Object/cours_exercice/corrigeretexercice/excebleCode/codeObject/compteBanque/ConsoleAppBouteille/Program.cs
@@ -0,0 +1,50 @@
+﻿using ClassLibraryBouteille;
+
+
+
+//Bouteille bouteille = new Bouteille();
+//Console.WriteLine(bouteille.ToString());
+
+//Bouteille bouteilleClone = new Bouteille(bouteille);
+
+//if (bouteille.Equals(bouteilleClone))
+//{
+//    Console.WriteLine("c'est egal");
+//}
+
+//int a = 0;                            code fait en cours
+
+
+//contenanceEnLitre contenuEnLitre estOuverte 
+Bouteille b;
+
+
+//contenanceEnLitre, contenuEnLitre, estOuverte 
+
+//Bouteille clone = new Bouteille(b);
+
+// Pour la méthode Ouvrir 
+//bool testOuvire = b.Ouvrir();
+
+// Pour la méthode fermer
+//bool testFermer = b.Fermer();
+
+// Pour la méthode vider
+//bool testVider = b.Vider();
+
+
+
+//bool testcremplir = b.Remplir();
+
+try
+{
+    b = new Bouteille(2, -1, true);
+    //Pour la méthode vider avec quantiter 
+    bool testVideravec = b.Vider(0.5);
+    //bool testcremplir = b.Remplir(-3);
+}
+catch (ArgumentException e)
+{
+    Console.WriteLine("Valeur negatif");
+}
+
