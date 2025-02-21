b3830f80afa268e19c4bc910b02f04183111e9be
BenjaminHerault <148880832+BenjaminHerault@users.noreply.github.com>
2025-02-21 11:53:18 +0100

    papillon code fait test fait test unitaire en cours

---
+++ 04-Object/cours_exercice/corrigeretexercice/papillon/papillon_de_lumier/bibliothequePapillon/Oeuf.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bibliothequePapillon
+{
+    public class Oeuf : StadeDEvolution
+    {
+        public Oeuf()
+        {
+            
+        }
+        public override void SeDeplacer()
+        {
+            Console.WriteLine("Je peux pas");
+        }
+        public override StadeDEvolution SeMetamorphoser()
+        {
+
+            return new Chenille();
+        }
+    }
+}
---
+++ 04-Object/cours_exercice/corrigeretexercice/papillon/papillon_de_lumier/papillon_de_lumier/Program.cs
@@ -0,0 +1,11 @@
+﻿using bibliothequePapillon;
+
+Lepidoptere lepi;
+lepi = new Lepidoptere("bombyx");
+lepi.SeDeplacer();      //oeuf
+lepi.SeMetamorphoser(); //chenile
+lepi.SeDeplacer();      
+lepi.SeMetamorphoser(); //chryalide
+lepi.SeDeplacer();
+lepi.SeMetamorphoser(); //  papillon
+lepi.SeDeplacer();
