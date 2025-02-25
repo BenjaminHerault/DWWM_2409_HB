6a796c84ab7bd81784c2d9666546c41980afc9f7
BenjaminHerault <148880832+BenjaminHerault@users.noreply.github.com>
2025-02-25 14:48:05 +0100

    introduction2 js fini et fonctionnele

--- 05-WebBack/exercicecorriger/IntroductionJs1/main.js
+++ 05-WebBack/exercicecorriger/IntroductionJs1/main.js
@@ -1,21 +1,24 @@
 function plus(){
     let nbclick= Number(document.querySelector("#compteur").textContent);
    
-    console.log(nbclick);
+    // console.log(nbclick); pour voir si sa marche
     document.getElementById('compteur').textContent = nbclick+1;
 }
 function rest(){
-   
+    nbclick= Number(document.querySelector("#compteur").textContent);
     document.getElementById('compteur').textContent = 0;
-}
+};
 
 
 
 const btn=document.querySelector("#boutonAjout");
-btn.addEventListener("click",plus
-  
-);
+btn.addEventListener("click",plus);
 
+const btnRei=document.querySelector("#boutonRei");
+btnRei.addEventListener("click",rest);
+
+// btn=document.querySelector("#boutonAjout");
+// btn.addEventListener("click",rest);
 /*function() {
    
     let nbclick= Number(document.querySelector("#compteur").textContent);
--- 05-WebBack/exercicecorriger/introduction2Js/main.js
+++ 05-WebBack/exercicecorriger/introduction2Js/main.js
@@ -1,7 +1,51 @@
-var plus1 = 16
+// const text = je l'apple comme je veux 
+// document.querySelector('.text') fait refference au html
+//const text(on nom dans le js) = document.querySelector("#texteTaille (et nome dans le html)")
+const textValue = document.querySelector("#texteTaille");
+const btnTaille = document.querySelector("#boutonTaille");
+const btnDimi = document.querySelector("#boutonDimi");
+const conteur   = document.querySelector("#numeroTaille");
+const nombreValue = document.querySelector("#saisieTaille");
 
-function plus(){
-    plus1++
-    document.querySelector("#numeroTaille").textContent = plus1
-    document.querySelector("#texteTaille")
+let taille = 16;
+conteur.innerHTML = taille
+
+function ajuster(taille){
+    conteur.innerHTML = taille
+    textValue.style.fontSize = taille + 'px'
 }
+function plus(){
+    if (taille <= 47){
+        taille++;
+        ajuster(taille)
+    }
+    else{
+        taille = 16;
+        ajuster(taille) 
+    }
+}
+function moin(){
+    if(taille > 8){
+        taille--;
+        ajuster(taille)
+    }
+    else{
+        taille = 16;
+        ajuster(taille)   
+    }
+}
+function changer(e){
+    const nouvelleTaille = parseInt(e.target.value);
+    if(nouvelleTaille >= 8 && nouvelleTaille <= 47){
+        taille = nouvelleTaille;
+        ajuster(taille)
+    }
+    else{
+        taille = 16;
+        ajuster(taille)
+    }
+
+}
+btnTaille.addEventListener("click", plus);
+btnDimi.addEventListener("click", moin);
+nombreValue.addEventListener("input", changer);
