861df0a3592126893dd4deefe5131be067a0fef8
BenjaminHerault <148880832+BenjaminHerault@users.noreply.github.com>
2025-02-27 10:01:42 +0100

    formulaires js fini

--- 05-WebBack/exercicecorriger/formulairesJs/asser/main.js
+++ 05-WebBack/exercicecorriger/formulairesJs/asser/main.js
@@ -3,21 +3,15 @@ const ageVerif = document.querySelector("#age");
 const validerVerif = document.querySelector("#boutonValider");
 const viderVerif = document.querySelector("#boutonVider");
 const messageVerif = document.querySelector("#texteInfo");
-const mainStyle = document.querySelector("main");
+
+
 
 let texte = "Compléter/corriger le Formulaire.";
-
 const regex = /^[a-zA-Z-]+$/g;
 
 const voirTexte = () => {
     messageVerif.innerHTML = texte;
 }
-
-const cssTest = () => {
-    mainStyle.style.backgroundColor = "red";
-}
-
-
 
 // function voirTexte(){ 
 //     messageVerif.innerHTML = texte;
@@ -29,42 +23,41 @@ const onVide = () =>{
 }
 
 const maRetraite = () => {
-    if(ageVerif.value < 64){
-        let annee = 64 - ageVerif.value;
-        return "Il vous reste "+annee+" année(s) avant la retraite";
+    if(ageVerif.value == 64){
+        return "Vous prenez votre retraite cette année !";
     }
    else if (ageVerif.value>64){
          let annee = ageVerif.value - 64;
          return "Vous êtes à la retraite depuis "+annee+" année(s)";
     }
     else{
-        return "Vous prenez votre retraite cette année !";
+        let annee = 64 - ageVerif.value;
+        return "Il vous reste <span style='color:blue;'><span style='font-weight:bold;'>"+annee+"</span></span> année(s) avant la retraite";
     }
 }
 
 const ageMajorite = () => {
     if(ageVerif.value < 18) {
-        return "Vous êtes mineur";
+        return "Vous êtes<span style='color:blue;'><span style='font-weight:bold;'> " +"mineur."+ "</span></span>";
     }
     else{
-        return "Vous êtes majeur";
+        return "Vous êtes <span style='color:blue;'><span style='font-weight:bold;' >"+"majeur."+ "</span></span>";
     }
 }
 
 const voirInfo = () => {
     if(prenomVerif.value.match(regex) && ageVerif.value != "" && ageVerif.value > 0){
-        messageVerif.innerHTML = "Bonjour " + prenomVerif.value + ", votre âge est " + ageVerif.value + "<br><br>"+ ageMajorite()+ "<br><br>" +  maRetraite();
+        messageVerif.innerHTML = "Bonjour <span style='color:blue;'><span style='font-weight:bold;'> " + prenomVerif.value + "</span></span>, votre âge est <span style='color:blue;'><span style='font-weight:bold;'>" + ageVerif.value + "</span></span><br><br>"+ ageMajorite()+ "<br><br>" +  maRetraite();
     }
     else{
         voirTexte();
     }
 }
-
 // avec la balice button en html qu'on utilise le preventDefault()
 // validerVerif.addEventListener("click",(e) => {
 //     e.preventDefault()});
 // validerVerif.addEventListener("click",voirTexte);
 validerVerif.addEventListener("click",voirInfo);
 viderVerif.addEventListener("click",onVide);
-mainStyle.addEventListener("",cssTest);
+
 
