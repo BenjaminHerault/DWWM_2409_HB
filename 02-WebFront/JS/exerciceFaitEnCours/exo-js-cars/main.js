6b2a6d5b0dab5d7f86c62477d31d40dfaa20e070
BenjaminHerault <148880832+BenjaminHerault@users.noreply.github.com>
2025-03-19 12:55:46 +0100

    exercice voiture fini et style aussi

--- 02-WebFront/JS/exerciceFaitEnCours/exo-js-cars/main.js
+++ 02-WebFront/JS/exerciceFaitEnCours/exo-js-cars/main.js
@@ -1,43 +1,56 @@
 const voiture = [];
 const voitureValide = document.getElementById("validate");
 const maVoiture = document.getElementById("carName");
-// const voitureTrouvee = false;
+const voitureTrouvee = false;
+const aucuneInfo = document.createElement("p");
+const infoVoiture =  document.createElement("p");
+const divResult = document.getElementById("result");
 
 fetch("./data/cars.json").then(response => response.json()).then(data => {
     for(let i = 0; i < data.length; i++){
-        voiture.push(data[i].car_name);
+        voiture.push(data[i].car_name.toLowerCase());
+    }
+voitureValide.addEventListener("click", function(event){
+    event.preventDefault();
+    infoVoiture.innerHTML = "";
+    maVoiture.value = maVoiture.value.trim().toLowerCase();
+
+    if(maVoiture.value.length < 1){
+        AucuneInfoTrouver();
+        return;
     }
 
-voitureValide.addEventListener("click", function(){
-    event.preventDefault();
-    console.log("un truc");
     let voitureTrouvee = false;
     for(let i = 0; i < data.length; i++){
-        if(voiture[i]=== maVoiture.value){
+        if(voiture[i].includes(maVoiture.value)){
             lesInfoVoiture(data[i]);
-            console.log("Voiture trouvée");
             voitureTrouvee = true;
-            break
         }
-
     };
     if(voitureTrouvee===false){
         console.log("Aucune voiture ne correspond à votre recherche");
+        AucuneInfoTrouver();
+        infoVoiture.remove();
     }
     });
 }).catch(error => console.error(error));
 
 
-function lesInfoVoiture(data){
-    let idVoicture = document.createElement("p");
-    let nomVoiture = document.createElement("p");
-    let anneeVoicture = document.createElement("p");
-    let paysOrigineVoiture = document.createElement("p");
+function AucuneInfoTrouver(){  
+    aucuneInfo.id = "aucuneInfo";
+    aucuneInfo.textContent = "Aucune voiture ne correspond à votre recherche";
+    divResult.appendChild(aucuneInfo);
+    setTimeout(() => {
+        aucuneInfo.remove();
+    }, 5000);
+}
 
-    for(let i = 0; i < data.length; i++){
-        idVoicture.textContent = "ID: " + data[i].car_id;
-        nomVoiture.textContent = "Nom: " + data[i].car_name;
-        anneeVoicture.textContent = "Année: " + data[i].car_model;
-        paysOrigineVoiture.textContent = "Pays d'origine: " + data[i].car_origin;
-    }
+function lesInfoVoiture(data){
+    infoVoiture.id = "infoVoiture";
+    infoVoiture.innerHTML += 
+    "<div class=\"resultat\">ID: " + data.car_id + 
+    "<br>Nom: " + data.car_name + 
+    "<br>Année: " + data.car_model + 
+    "<br>Pays d'origine: " + data.car_origin +"</div>";
+    divResult.appendChild(infoVoiture);
 }
