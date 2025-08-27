a11340e935faf68a2b9b7efe439bb7b3ecbf6d47
BenjaminHerault <148880832+BenjaminHerault@users.noreply.github.com>
2025-08-27 14:01:45 +0200

    exercice fini ecf entrainement

--- 02-WebFront/js_eval_blanc/assets/js/Controler_saisies.js
+++ 02-WebFront/js_eval_blanc/assets/js/Controler_saisies.js
@@ -1,6 +1,21 @@
 export class Control_saisies {
-    constructor(_username, _password) {
+    constructor(_username, _password, _passwordVerif) {
         this.username = _username;
-        this.password = _username;
+        this.password = _password;
+        this.passwordVerif = _passwordVerif;
+
+        if (this.username.length < 3) {
+            throw new Error(
+                "Le nom d'utilisateur est trop court (3 caractères minimum)"
+            );
+        }
+        if (this.password.length < 12) {
+            throw new Error(
+                "Le mot de passe est trop court (12 caracttères minimum)"
+            );
+        }
+        if (this.password !== this.passwordVerif) {
+            throw new Error("Les mots de passe ne sont pas identiques.");
+        }
     }
 }
--- 02-WebFront/js_eval_blanc/assets/js/main.js
+++ 02-WebFront/js_eval_blanc/assets/js/main.js
@@ -5,9 +5,50 @@ const monApp = {
         return {
             nom: "Herault",
             prenom: "Benjamin",
+            nomUtilisateur: "",
+            motDePasse: "",
+            motDePasseVerif: "",
+            erreur: "",
         };
     },
-    async created() {
-        let response = await fetch();
+    methods: {
+        verifierFormulaire() {
+            // Vérification nom et mot de passe
+            try {
+                new Control_saisies(
+                    this.nomUtilisateur,
+                    this.motDePasse,
+                    this.motDePasseVerif
+                );
+            } catch (e) {
+                this.erreur = e.message;
+                setTimeout(() => {
+                    this.erreur = "";
+                }, 5000);
+                return;
+            }
+            // Si tout est OK
+            this.erreur = "Le formulaire est valide !";
+            document.body.style.backgroundColor = "#006400";
+            document.querySelector("fieldset.form").style.backgroundColor =
+                "#338333";
+            document.querySelectorAll("input.couleurs").forEach((input) => {
+                input.style.backgroundColor = "#5c9c5c";
+            });
+            document.querySelectorAll("button.couleurs").forEach((input) => {
+                input.style.backgroundColor = "#5c9c5c";
+            });
+            this.userNew = {
+                username: this.nomUtilisateur,
+                password: this.motDePasse,
+            };
+            this.affichageUser = `Nom d'utilisateur : ${this.userNew.username} | Mot de passe : ${this.userNew.password}`;
+            setTimeout(() => {
+                this.erreur = "";
+            }, 5000);
+        },
     },
 };
+
+const vm = Vue.createApp(monApp);
+vm.mount("#app");
